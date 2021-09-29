bfc11ec19cd63b690cc0ea82997976a0ec711413
joaquuin <joaquin96.RM@gmail.com>
2021-09-29 11:57:19 +0200

    probando

--- Desarrollo_Cliente/Selectores/Ejercicio2.js
+++
@@ -1,9 +0,0 @@
-// Crear el fichero Ejercicio2.js con el código necesario para que cuando se pinche sobre el enlace de la página Ejercicio2.html
-// se muestre completo el contenido de texto. Además, el enlace debe dejar de mostrarse después de pulsarlo por primera vez.
-
-document.getElementById("enlace").addEventListener('click', mostrar);
-
-function mostrar() {
-    document.getElementById("adicional").classList.add("visible");
-    document.getElementById("enlace").classList.add("oculto");
-}
--- Desarrollo_Cliente/Selectores/Ejercicio3.js
+++
@@ -1,9 +0,0 @@
-let boton = document.getElementById("colorear").addEventListener('click', colorear);
-
-function colorear() {
-    let fila = document.getElementById("fil").value;
-    let columna = document.getElementById("col").value;
-
-    let pos = document.querySelectorAll("tr")[fila - 1].querySelectorAll("td")[columna - 1].classList.add("rojo");
-
-}
