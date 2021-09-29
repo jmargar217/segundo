fc44de828536e4b1187bbdac0f6716cd35b1dfcf
joaquuin <joaquin96.RM@gmail.com>
2021-09-29 14:18:43 +0200

    Empezado tarea formulario

---
+++ Desarrollo_Cliente/formularios/formulario1.js
@@ -0,0 +1,15 @@
+/*
+Formulario con expresiones regulares y condicionales que hagan aparecer preguntas en función de la respuesta
+*/
+
+
+const formulario = document.querySelector("form");
+const validoFormu = {
+    nombre: false,
+    apellidos: false,
+    nacimiento: false,
+    coches: false,
+    hijos: false
+}
+
+
