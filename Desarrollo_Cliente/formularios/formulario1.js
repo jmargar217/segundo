2d37b3eee951cc8a828dec2bd21fe4362823690f
Joaquin <joaquin96.RM@gmail.com>
2021-09-29 19:41:22 +0200

    cambios formularios

--- Desarrollo_Cliente/formularios/formulario1.js
+++ Desarrollo_Cliente/formularios/formulario1.js
@@ -4,12 +4,49 @@ Formulario con expresiones regulares y condicionales que hagan aparecer pregunta
 
 
 const formulario = document.querySelector("form");
-const validoFormu = {
+const nombre = document.getElementById("nombre");
+const apellidos = document.getElementById("apellido");
+const dni = document.getElementById("dni");
+const coches = document.getElementById("coches");
+const hijos = document.getElementById("hijos");
+
+const regExTexto = /^([A-Z][a-z]+)/mg;
+const regExDni = /^([1-9][0-9]{7}[A-Z]+)/mg;
+const regExNumeros = /^([1-9]*)/mg;
+
+const formuValido = {
     nombre: false,
-    apellidos: false,
-    nacimiento: false,
-    coches: false,
-    hijos: false
+    //apellidos: false,
+    //dni: false,
+    //coches: false,
+    //hijos: false
 }
 
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    validarFormulario();
+})
 
+
+nombre.addEventListener('change', (e) => {
+    e.preventDefault();
+
+    if (e.target.value.trim().length > 0 && regExTexto.test(e.target.value)) {
+        formuValido.nombre = true;
+    } else {
+        alert("Nombre incorrecto");
+    }
+
+})
+
+
+function validarFormulario() {
+    const formValues = Object.values(formuValido);
+    const valid = formValues.findIndex(value => value == false);
+    if (valid == -1) {
+        formulario.submit();
+    } else {
+        alert("Datos no válidos");
+    }
+
+}
