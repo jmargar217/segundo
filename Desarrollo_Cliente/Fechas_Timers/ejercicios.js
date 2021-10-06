271a0af068f74f53f8751078ff49a6fb51669417
Joaquin <joaquin96.RM@gmail.com>
2021-10-06 20:46:08 +0200

    .

--- Desarrollo_Cliente/Fechas_Timers/ejercicios.js
+++ Desarrollo_Cliente/Fechas_Timers/ejercicios.js
@@ -2,14 +2,14 @@
 1. Un campo que nos permite seleccionar la fecha y hora y la muestra a continuación. La salida deberá tener elsiguiente formato:
 Hoy es martes, 28 de Febrero de 2018 y son las 14:32 horas.
 */
-const dias = ['domingo','lunes','martes','miércoles','jueves','viernes','sabado'];
-const meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+const dias = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sabado'];
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
-const fecha = document.getElementById("fecha").addEventListener("change",(e)=>{
+const fecha = document.getElementById("fecha").addEventListener("change", (e) => {
     const aux = new Date(document.getElementById("fecha").value);
-    document.getElementById("texto").innerText= 'Hoy es '+dias[aux.getDay()]+", "+aux.getDate()+" de "+meses[aux.getMonth()]+" y son las "+
-    aux.getHours()+":"+aux.getMinutes();
-    
+    document.getElementById("uno").innerText = 'Hoy es ' + dias[aux.getDay()] + ", " + aux.getDate() + " de " + meses[aux.getMonth()] + " y son las " +
+        aux.getHours() + ":" + aux.getMinutes();
+
 })
 
 
@@ -21,19 +21,19 @@ OCTUBRE – 2014
 1 (miercoles), 2 (jueves), ….. , 31 (viernes).
 */
 
-const boton = document.getElementById("calendario").addEventListener("click",(e)=>{
+const boton = document.getElementById("calendario").addEventListener("click", (e) => {
     let mes = document.getElementById("mes").value;
     let year = document.getElementById("year").value;
 
-    if(mes >0 && mes <13 ){
-      let fechap = new Date(year, mes-1,1);
-      let auxt = fechap.getMonth();
+    if (mes > 0 && mes < 13) {
+        let fechap = new Date(year, mes - 1, 1);
+        let auxt = fechap.getMonth();
 
-      while(fechap.getMonth() == auxt){
-           
-          document.getElementById("prueba").innerText += fechap.getDate()+" ("+dias[fechap.getDay()]+"), ";
-          fechap.setDate(fechap.getDate()+1) ;
-      }
+        while (fechap.getMonth() == auxt) {
+
+            document.getElementById("dos").innerText += fechap.getDate() + " (" + dias[fechap.getDay()] + "), ";
+            fechap.setDate(fechap.getDate() + 1);
+        }
     }
 
 })
@@ -44,20 +44,59 @@ const boton = document.getElementById("calendario").addEventListener("click",(e)
 una ves introducidos nos devolverá la fecha del pago.
 */
 
-const pago = document.getElementById("pago").addEventListener("click",(e)=>{
+const pago = document.getElementById("pago").addEventListener("click", (e) => {
     let fechita = new Date(document.getElementById("fechita").value);
     let numDias = document.getElementById("numDias").value;
 
-    fechita.setDate(fechita.getDate()+parseInt(numDias));
-    fechita.setMonth(fechita.getMonth()+parseInt(1));
+    fechita.setDate(fechita.getDate() + parseInt(numDias));
+    fechita.setMonth(fechita.getMonth() + 1);
 
-    document.getElementById("prueba2").innerText = "La fecha de pago será el día "+fechita.getDate()+"/"+fechita.getMonth()+"/"+fechita.getFullYear();
+    document.getElementById("tres").innerText = "La fecha de pago será el día " + fechita.getDate() + "/" + fechita.getMonth() + "/" + fechita.getFullYear();
 
 })
 
+/*
+4. Un cronómetro con el siguiente formato:
+Hoy es 30-9-2019 y son las 21:4:23 horas.
+*/
+
+function cronometro() {
+    let fechaActual = new Date();
+    document.getElementById("cuatro").innerText = "Hoy es " + fechaActual.getDate() + "-" + fechaActual.getMonth() + "-" + fechaActual.getFullYear() +
+        " y son las " + fechaActual.getHours() + ":" + fechaActual.getMinutes() + ":" + fechaActual.getSeconds() + " horas.";
+
+}
+
+setInterval(cronometro, 100);
+
+
+/*
+5. Una alarma que mostrará la hora actual y nos permite indicar la hora a la que sonará, cuando llegue a la hora,
+pregunta si se quiere posponer o detener, si se pospone "sonará" a los 2 minutos.
+EXTRA: haz que suene un sonido además de mostrar el mensaje.
+*/
+
+
+function sonar() {
+    let horaActual = new Date();
+    document.getElementById("cinco").innerText = "La hora actual es: " + horaActual.getHours() + ":" + horaActual.getMinutes() + ":" + horaActual.getSeconds();
+
+    let inputTime = document.getElementById("hora").value;
+    let aux = inputTime.split(":");
+    let horaDada = new Date(0, 0, 0, aux[0], aux[1], 0, 0);
+
+    if (horaActual.getHours() == horaDada.getHours() &&
+        horaActual.getMinutes() == horaDada.getMinutes()) {
+
+        let respuesta = prompt("¿Quieres posponer(P) o detener(D) la alarma?");
+        if (respuesta == "P") {
+            setTimeout(document.getElementById("seis").innerText = "ola", 120000);
+        } else {
+            document.getElementById("seis").innerText = "Alarma sonandooo!";
+        }
+    }
+}
 
 
 
-
-
-
+setInterval(sonar, 1000);
