3453a9c46845662a086f069ca70bd5799d362ac1
joaquuin <joaquin96.RM@gmail.com>
2021-10-13 14:16:04 +0200

    clase 13-Octubre

---
+++ Desarrollo_Cliente/AJAX/ejemploAJAX.js
@@ -0,0 +1,27 @@
+let peticion = new XMLHttpRequest();
+
+console.log("Estado inicial de la petición: " + peticion.readyState);
+peticion.open('GET', 'http://localhost:3000/posts');
+
+console.log("Estado de la petición tras el 'open': " + peticion.readyState);
+peticion.send();
+console.log("Petición hecha");
+
+peticion.addEventListener('readystatechange', function() {
+console.log("Estado de la petición: " + peticion.readyState);
+if (peticion.readyState === 4) {
+    if (peticion.status === 200) {
+        console.log("Datos recibidos:");
+        let usuarios = JSON.parse(peticion.responseText); // Convertirmos los datos JSON a un objeto
+        console.log(usuarios);
+} else {
+    console.log("Error " + peticion.status + " (" + peticion.statusText + ") en la petición");
+}
+}
+})
+
+console.log("Petición acabada");
+
+
+
+// Sirve para mostrar los productos que tenemos almacenados
