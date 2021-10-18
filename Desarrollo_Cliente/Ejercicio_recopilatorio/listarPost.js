893a7dd4dd5e4cf51f53c257d5929135eae6ec7d
joaquuin <joaquin96.RM@gmail.com>
2021-10-18 14:19:36 +0200

    clase 18 octubre

--- Desarrollo_Cliente/Ejercicio_recopilatorio/listarPost.js
+++ Desarrollo_Cliente/Ejercicio_recopilatorio/listarPost.js
@@ -4,27 +4,32 @@ function cargarLista() {
     let tabla = document.getElementById("tabla");
     let peticion = new XMLHttpRequest();
     console.log("Estado inicial de la petición: " + peticion.readyState);
-    peticion.open("GET", "http://localhost:3000/posts", false);
+    peticion.open("GET", "http://localhost:3000/posts");
     console.log("Estado de la petición tras el 'open': " + peticion.readyState);
     peticion.send();
-    if (peticion.readyState == 4) {
+    peticion.addEventListener('load', () =>{
+    
         if (peticion.status == 200) {
             console.log("Datos recibidos:");
             let posts = JSON.parse(peticion.responseText); // Convertirmos los datos JSON a un objeto
             for (let i in posts) {
                 if (i != 0) {
                     let fila = document.createElement("tr");
-                    let cuno = document.createElement("td")
-                    let cdos = document.createElement("td")
-                    let ctres = document.createElement("td")
-                    let ccuatro = document.createElement("td")
+                    let cuno = document.createElement("td");
+                    let cdos = document.createElement("td");
+                    let ctres = document.createElement("td");
+                    let ccuatro = document.createElement("td");
 
-                    let borrar = document.createElement("input");
-                    borrar.setAttribute("type", "button");
-                    borrar.setAttribute("value", "Borrar");
-                    let ver = document.createElement("input");
-                    ver.setAttribute("type", "button");
-                    ver.setAttribute("value", "Ver");
+                    let borrar = document.createElement("a");
+                    borrar.innerText = "Borrar"
+                    borrar.setAttribute("name","Borrar");
+                    borrar.setAttribute("href","");
+                    borrar.setAttribute("id",posts[i].id);
+
+                    let ver = document.createElement("a");
+                    ver.innerText = "Ver";
+                    ver.setAttribute("href","http://localhost:3000/posts/"+posts[i].id);
+                    ver.setAttribute("id", posts[i].id);
 
 
                     let id = document.createTextNode(posts[i].id);
@@ -47,13 +52,36 @@ function cargarLista() {
         } else {
             console.log("Error " + peticion.status + " (" + peticion.statusText + ") en la petición");
         }
-        tabla.addEventListener("click", (e) => {
-            if (e.target.value == "Borrar") {
+        
+        tabla.addEventListener("click",(e)=>{
+            if (e.target.name == "Borrar"){
+                let peticionDos = new XMLHttpRequest();
+                peticionDos.open("DELETE", "http://localhost:3000/posts/"+e.target.id);
+                peticionDos.send();
 
-            } else if (e.target.value == "Ver") {
+                peticionDos.addEventListener('load',function(){
+                    location.reload();
+
+                })
+                
+            } else if(e.target.name=="Ver"){
+                let peticionDos = new XMLHttpRequest();
+                peticionDos.open("GET", "http://localhost:3000/posts/"+e.target.id);
+                peticionDos.send();
+                peticionDos.addEventListener('load',function(){
+                    let aux = JSON.parse(peticionDos.responseText);
+                    document.innerText = aux.titulo;
+
+                    //href = posible.html?postid=5"  recupera parametros  url javascript
+
+                })
+
 
             }
-        });
-    }
+            
+        })
+    
+})
+
 
 }
