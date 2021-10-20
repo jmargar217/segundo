4c056787ea766473fbfb0b7b7637b454e4ec2bc8
joaquuin <joaquin96.RM@gmail.com>
2021-10-20 13:50:14 +0200

    clase 20 octubre

--- Desarrollo_Cliente/Ejercicio_recopilatorio/modificar.js
+++ Desarrollo_Cliente/Ejercicio_recopilatorio/modificar.js
@@ -3,6 +3,25 @@ const parametros = new URLSearchParams(valores);
 let id = parametros.get("id");
 const enviar = document.getElementById("enviar");
 
+let peticion = new XMLHttpRequest();
+    console.log("Estado inicial de la petición: " + peticion.readyState);
+    peticion.open("GET", "http://localhost:3000/posts/"+id);
+    console.log("Estado de la petición tras el 'open': " + peticion.readyState);
+    peticion.send();
+    peticion.addEventListener('load', () =>{
+        if (peticion.readyState === 4) {
+            if(peticion.status===200){
+                let post = JSON.parse(peticion.responseText);
+                document.getElementById("titulo").setAttribute("value",post.titulo);
+                document.getElementById("contenido").value=post.contenido;
+            }
+        }
+
+    });
+
+
+
+
 enviar.addEventListener('click',()=>{
     let tituloo = document.getElementById("titulo").value;
     let contenidoo = document.getElementById("contenido").value;
