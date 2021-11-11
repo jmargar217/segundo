0c484a739d6558e03a08e7919af6105cdcb2887f
Joaquin <joaquin96.RM@gmail.com>
2021-11-11 19:15:47 +0100

    Creado proyecto prueba con angular

---
+++ Desarrollo_Cliente/proyectoAngular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ListadoComponent } from './views/listado/listado.component';
+import { EntradaComponent } from './views/listado/entrada/entrada.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    ListadoComponent,
+    EntradaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Desarrollo_Cliente/proyectoAngular/src/app/views/listado/listado.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Entrada } from 'src/app/shared/interfaces/entrada';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent implements OnInit {
+  // Atibutos
+  public listadoEntradas: Entrada[];
+
+  constructor() {
+    this.listadoEntradas = [
+      {
+        titulo: 'Entrada 1',
+        resumen: 'Ejemplo de entrada'
+      },
+      {
+        titulo: 'Entrada 2',
+        resumen: 'Ejemplo de entrada'
+      },
+      {
+        titulo: 'Entrada 3',
+        resumen: 'Ejemplo de entrada'
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+  }
+
+  public mostrarTitulo(titulo: string): void {
+    alert(`Entrada seleccionada: ${ titulo }.`);
+  }
+
+}
